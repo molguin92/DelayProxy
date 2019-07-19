85c0c3d281e78b1493adce8eded36635f7f7ed88
Manuel Osvaldo Olguín <molguin@kth.se>
2019-07-19 18:13:25 +0200

    base implementation and cli!

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 #  Copyright 2019 Manuel Olguín Muñoz <manuel@olguin.se><molguin@kth.se>
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,21 +13,82 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+import enum
+import ipaddress
+import signal
+from multiprocessing import Event
+from typing import List, Optional
+
 import click
+
+from proxy import BaseDelayProxy, ExponentialDelayProxy
+
+
+class INetAddress(click.ParamType):
+    class TYPE(enum.IntEnum):
+        TO = 0
+        FROM = 1
+
+    def __init__(self, to_or_from: Optional[TYPE] = None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.name = {
+            INetAddress.TYPE.TO  : 'HOST_ADDRESS:HOST_PORT',
+            INetAddress.TYPE.FROM: 'BIND_ADDRESS:BIND_PORT'
+        }.get(to_or_from, 'ADDRESS:PORT')
+
+    def convert(self, value, param, ctx):
+        try:
+            [ip, port] = value.split(':')
+            ipaddress.ip_address(ip)
+            port = int(port)
+            assert port < 65535
+        except Exception:
+            self.fail(f'{value} is not a valid IPv4 or IPv6 address.',
+                      param, ctx)
+        return ip, port
 
 
 @click.command()
-@click.option('-n', '--number_proxies',
-              default=1,
-              type=int,
-              help='Number of proxies to execute')
+@click.option('-v', '--verbose',
+              count=True, type=int, default=0,
+              help='Logging verbosity.')
 @click.option('-p', '--proxy',
               required=True,
-              type=str,
+              type=click.Tuple([INetAddress(INetAddress.TYPE.FROM),
+                                INetAddress(INetAddress.TYPE.TO)]),
               nargs=2,
-              multiple=True)
-def main():
-    pass
+              multiple=True,
+              help='\
+Hosts to relay packets between, can be provided multiple times to specify \
+multiple relays. The application will bind and listen for an incoming \
+connection on BIND_ADDRESS:BIND_PORT and relay all data coming from it to \
+HOST_ADDRESS:HOST_PORT.'
+              )
+def main(verbose, proxy):
+    proxies: List[BaseDelayProxy] = list()
+
+    def __sig_handler(*args, **kwargs):
+        for p in proxies:
+            p.stop()
+        exit(0)
+
+    signal.signal(signal.SIGINT, __sig_handler)
+
+    for addr_a, addr_b in proxy:
+        print(f'Setting up relay from {addr_a} to {addr_b}...')
+        listen_host, listen_port = addr_a
+        connect_host, connect_port = addr_b
+        proxies.append(ExponentialDelayProxy(
+            scale=0.1,
+            listen_host=listen_host,
+            listen_port=listen_port,
+            connect_host=connect_host,
+            connect_port=connect_port))
+
+    for p in proxies:
+        p.start()
+
+    Event().wait()  # wait forever
 
 
 if __name__ == '__main__':
--- proxy.py
+++ proxy.py
@@ -14,14 +14,46 @@
 import socket
 import time
 import numpy as np
-from multiprocessing import Event, Pool
+from multiprocessing import Event, Pool, Barrier
 from multiprocessing.pool import AsyncResult
-from typing import Callable, Union
-from aiologger import Logger
+from typing import Callable, Union, Optional
+from functools import partial
+
+
+def constant_0():
+    return 0
+
+
+def pool_init(shutdown_signal: Event):
+    global shutdown_event
+    shutdown_event = shutdown_signal
+
+
+def relay(conn_A: socket.SocketType,
+          conn_B: socket.SocketType,
+          barrier: Barrier = None,
+          chunk_size: int = 4096,
+          delay_dist: Callable[[], float] = constant_0) -> None:
+    try:
+        while not shutdown_event.is_set():
+            # read from A, wait X time, send to B
+            data = conn_A.recv(chunk_size)
+            time.sleep(max(delay_dist(), 0.0))
+            conn_B.sendall(data)
+    except socket.error:
+        # todo: log error
+        pass
+    except Exception as e:
+        print(e)
+
+    if barrier:
+        barrier.wait()
 
 
 class BaseDelayProxy:
     SHUTDOWN_TIMEOUT = 3
+
+    __relay = staticmethod(relay)
 
     def __init__(self,
                  listen_host: str = '0.0.0.0',
@@ -29,70 +61,68 @@ class BaseDelayProxy:
                  connect_host: str = '0.0.0.0',
                  connect_port: int = 5001,
                  chunk_size: int = 4096,
-                 delay_dist: Callable[[], float] = lambda: 0):
+                 barrier: Barrier = None,
+                 delay_dist: Callable[[], float] = constant_0):
         super().__init__()
         self.listen_addr = (listen_host, listen_port)
         self.connect_addr = (connect_host, connect_port)
-        self.log = Logger.with_default_handlers(name=self.__name__)
         self.shutdown_signal = Event()
         self.delay_dist = delay_dist
         self.chunk_size = chunk_size
 
-        self.result: AsyncResult = None
-        self.ppool: Pool = None
-        self.conn_A: socket.SocketType = None
-        self.conn_B: socket.SocketType = None
-
-    @staticmethod
-    def __relay(conn_A: socket.SocketType,
-                conn_B: socket.SocketType,
-                chunk_size: int = 4096,
-                delay_dist: Callable[[], float] = lambda: 0,
-                shutdown_event: Event = Event()) -> None:
-        shutdown_event.clear()
-        while not shutdown_event.is_set():
-            # read from A, wait X time, send to B
-            data = conn_A.recv(chunk_size)
-            time.sleep(delay_dist())
-            conn_B.sendall(data)
+        self.result_AtoB: Optional[AsyncResult] = None
+        self.result_BtoA: Optional[AsyncResult] = None
+        self.ppool: Optional[Pool] = None
+        self.conn_A: Optional[socket.SocketType] = None
+        self.conn_B: Optional[socket.SocketType] = None
+        self.barrier = barrier
 
     def __setup(self) -> Union[socket.SocketType, socket.SocketType]:
         l_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        l_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         l_sock.bind(self.listen_addr)
         l_sock.listen(0)
         self.conn_A, addr_A = l_sock.accept()
-        self.log.info(f'Accepted connection from {addr_A}')
+        # self.log.info(f'Accepted connection from {addr_A}')
 
         self.conn_B = socket.create_connection(self.connect_addr)
-        self.log.info(f'Connected to {self.connect_addr}')
+        # self.log.info(f'Connected to {self.connect_addr}')
 
     def start(self) -> None:
         self.__setup()
-        self.ppool = Pool(2)
-        self.result = self.ppool.apply_async(
+        self.ppool = Pool(2,
+                          initializer=pool_init,
+                          initargs=(self.shutdown_signal,))
+        self.result_AtoB = self.ppool.apply_async(
             BaseDelayProxy.__relay,
             (self.conn_A, self.conn_B),
-            {'chunk_size'    : self.chunk_size,
-             'shutdown_event': self.shutdown_signal,
-             'delay_dist'    : self.delay_dist})
+            {'chunk_size': self.chunk_size,
+             'delay_dist': self.delay_dist})
+        self.result_BtoA = self.ppool.apply_async(
+            BaseDelayProxy.__relay,
+            (self.conn_B, self.conn_A),
+            {'chunk_size': self.chunk_size,
+             'delay_dist': self.delay_dist})
         self.ppool.close()
 
     def stop(self):
         self.shutdown_signal.set()
-        if self.conn_A:
-            self.conn_A.shutdown(socket.SHUT_RDWR)
-            self.conn_A.close()
+        try:
+            if self.conn_A:
+                self.conn_A.shutdown(socket.SHUT_RDWR)
+                self.conn_A.close()
+        except OSError:
+            pass
 
-        if self.conn_B:
-            self.conn_B.shutdown(socket.SHUT_RDWR)
-            self.conn_B.close()
+        try:
+            if self.conn_B:
+                self.conn_B.shutdown(socket.SHUT_RDWR)
+                self.conn_B.close()
+        except OSError:
+            pass
 
-        if self.result and self.ppool:
-            try:
-                self.result.get(self.SHUTDOWN_TIMEOUT)
-            except TimeoutError:
-                self.ppool.terminate()
-
+        if self.ppool:
+            self.ppool.terminate()
             self.ppool.join()
 
 
@@ -111,7 +141,7 @@ class NormalDelayProxy(BaseDelayProxy):
             connect_host=connect_host,
             connect_port=connect_port,
             chunk_size=chunk_size,
-            delay_dist=lambda: np.random.normal(mean, std_dev))
+            delay_dist=partial(np.random.normal, loc=mean, scale=std_dev))
 
 
 class ExponentialDelayProxy(BaseDelayProxy):
@@ -128,4 +158,4 @@ class ExponentialDelayProxy(BaseDelayProxy):
             connect_host=connect_host,
             connect_port=connect_port,
             chunk_size=chunk_size,
-            delay_dist=lambda: np.random.exponential(scale=scale))
+            delay_dist=partial(np.random.exponential, scale=scale))
